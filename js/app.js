0cb3a35664420be8fb860034cb1ee48e6e7bf53b
kaytee12 <kofi.taylor07@gmail.com>
2021-03-24 02:32:36 +0000

    using js to get bar to load

--- js/app.js
+++ js/app.js
@@ -1,1 +1,36 @@
+const loadingBarEl = document.querySelector(".loading-bar__item");
+const buttonEl = document.querySelectorAll("button")
 
+// let width = 1;
+// let loadingBar = setInterval(function(){
+//     if(width >= 100){
+//       clearInterval(loadingBar);
+//     }else{
+//          width++;
+//          loadingBarEl.style.width = width + "%";
+//     }
+// }, 9);
+
+
+function loadingPoint(){
+    for (let button of buttonEl){
+        console.log(button)
+         if(button.classList.contains("btn-30")){
+             button.addEventListener("click",function(){
+                 loadingBarEl.style.width = "30%";
+             })
+         }else if(button.classList.contains("btn-60")){
+               button.addEventListener("click",function(){
+                 loadingBarEl.style.width = "60%";
+             })
+         }else if(button.classList.contains("btn-90")){
+               button.addEventListener("click",function(){
+                 loadingBarEl.style.width = "90%";
+               })
+         }else{
+               loadingBarEl.style.width = "10%";
+         }
+    }
+}
+
+loadingPoint()
